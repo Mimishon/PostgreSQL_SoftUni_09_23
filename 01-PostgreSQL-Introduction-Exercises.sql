efd2d34c4736d987eeba2cade8ef00a8c26442a2
Mimishon <Mimishon@users.noreplay.github.com>
2023-09-30 16:47:13 +0300

    Update 01-PostgreSQL-Introduction-Exercises.sql

--- 01-PostgreSQL-Introduction-Exercises.sql
+++ 01-PostgreSQL-Introduction-Exercises.sql
@@ -20,6 +20,8 @@ CREATE TABLE IF NOT EXISTS minions(
 -- To avoid confusion, rename the newly created table to "minions_info". 
 -- Submit your query for this task in the Judge system.
 
+ALTER TABLE minions
+RENAME TO minions_info;
 
 --     3. Alter the Table
 -- As Gru builds a new weapon, he must delegate responsibilities to the Minions and give them tasks. For this reason, you need to update the table by adding new columns:
@@ -28,47 +30,79 @@ CREATE TABLE IF NOT EXISTS minions(
 --     • "salary" - specify to the third decimal place and has 8 digits;
 -- Submit your query for this task in the Judge system.
 
-
+ALTER TABLE minions_info
+ADD COLUMN code CHAR(4),
+ADD COLUMN task TEXT,
+ADD COLUMN salary NUMERIC(8,3);
 
 --     4. Rename Column
 -- Gru does not like the name of the column "salary", so he wants to change the name of this column to "banana".
 -- Submit your query for this task in the Judge system.
 
+ALTER TABLE minions_info
+RENAME COLUMN salary TO banana;
 
-    5. Add New Columns
-You need to update the table again because Gru forgot to mention that he needs to store emails to send Minions their schedules for the week. He also asks for information on whether Minions have the necessary equipment. Add two new columns with the following data:
-    • "email" - with a max length of 20 characters;
-    • "equipped" - state of true or false;
-*** Note, PostgreSQL offers three states of BOOLEAN data type: TRUE, FALSE, or NULL. Therefore, add a constraint to the BOOLEAN type.
-Submit your query for this task in the Judge system.
-    6. Create ENUM Type
-An enum type is a data type whose value is chosen from an enumerated list of possibilities. This will help Gru to understand how the Minions feel. Create type "type_mood" that contains the following set of values:
-    • ('happy', 'relaxed', 'stressed', 'sad')
-In the table add another column "mood" of type "type_mood".
-Submit your query for this task in the Judge system.
-    7. Set Default
-To maintain data accuracy and reliability in the table, it is necessary to establish specific rules. By default, columns allow NULL values. To enforce the columns to disallow NULL values, the following rules should be implemented:
-    • "age" - should have a DEFAULT 0;
-    • "name" and "code" - should have an empty string as the default value;
-*** Note, in this case, you need to use ALTER COLUMN to update/edit specific columns.
-Submit your query for this task in the Judge system.
-    8. Add Constraints
-Constraints can be specified when the table is created or afterward. Please use the syntax below to add constraints to the given columns:
-ALTER TABLE <table_name>
-ADD CONSTRAINT <constraint_name> <constraint_definition> (<column1>, <column2>, ... <column_n>);
-Add them to the following columns:
-    • "id" and "email"- should be UNIQUE across all the table rows. The constraint name for this column is "unique_containt";
-    • "banana" - should always be a positive number and greater than 0. The constraint name for this column is " banana_check";
-Submit your query for this task in the Judge system.
-    9. Change Column’s Data Type
-Gru has noticed that Dr. Nefario is taking too much time to write about the daily tasks of the Minions. He wants to change the data type of the "task" column and limit its length to 150 characters. 
-Submit your query for this task in the Judge system.
-    10. Drop Constraint
-Gru has realized that he needs to give the Minions two days off per week and wants to update the database accordingly. To do this, you need to remove the constraint that prevents the "equipped" column from having a NULL value.
-Submit your query for this task in the Judge system.
-    11. Remove Column
-Dr. Nefario decided to create another table and move some data there. Help him by writing a SQL query to DROP the column "age".
-Submit your query for this task in the Judge system.
+--     5. Add New Columns
+-- You need to update the table again because Gru forgot to mention that he needs to store emails to send Minions their schedules for the week. He also asks for information on whether Minions have the necessary equipment. Add two new columns with the following data:
+--     • "email" - with a max length of 20 characters;
+--     • "equipped" - state of true or false;
+-- *** Note, PostgreSQL offers three states of BOOLEAN data type: TRUE, FALSE, or NULL. Therefore, add a constraint to the BOOLEAN type.
+-- Submit your query for this task in the Judge system.
+
+ALTER TABLE minions_info
+ADD COLUMN email VARCHAR(20),
+ADD COLUMN equipped BOOLEAN NOT NULL;
+
+--     6. Create ENUM Type
+-- An enum type is a data type whose value is chosen from an enumerated list of possibilities. This will help Gru to understand how the Minions feel. Create type "type_mood" that contains the following set of values:
+--     • ('happy', 'relaxed', 'stressed', 'sad')
+-- In the table add another column "mood" of type "type_mood".
+-- Submit your query for this task in the Judge system.
+
+CREATE TYPE type_mood
+AS ENUM('happy', 'relaxed', 'stressed', 'sad');
+
+ALTER TABLE minions_info 
+ADD COLUMN mood "type_mood";
+
+
+--     7. Set Default
+-- To maintain data accuracy and reliability in the table, it is necessary to establish specific rules. By default, columns allow NULL values. To enforce the columns to disallow NULL values, the following rules should be implemented:
+--     • "age" - should have a DEFAULT 0;
+--     • "name" and "code" - should have an empty string as the default value;
+-- *** Note, in this case, you need to use ALTER COLUMN to update/edit specific columns.
+-- Submit your query for this task in the Judge system.
+
+ALTER TABLE minions_info 
+ALTER COLUMN age SET DEFAULT 0,
+ALTER COLUMN "name" SET DEFAULT '',
+ALTER COLUMN CODE SET DEFAULT '';
+
+--     8. Add Constraints
+-- Constraints can be specified when the table is created or afterward. Please use the syntax below to add constraints to the given columns:
+-- ALTER TABLE <table_name>
+-- ADD CONSTRAINT <constraint_name> <constraint_definition> (<column1>, <column2>, ... <column_n>);
+-- Add them to the following columns:
+--     • "id" and "email"- should be UNIQUE across all the table rows. The constraint name for this column is "unique_containt";
+--     • "banana" - should always be a positive number and greater than 0. The constraint name for this column is " banana_check";
+-- Submit your query for this task in the Judge system.
+
+
+--     9. Change Column’s Data Type
+-- Gru has noticed that Dr. Nefario is taking too much time to write about the daily tasks of the Minions. He wants to change the data type of the "task" column and limit its length to 150 characters. 
+-- Submit your query for this task in the Judge system.
+
+
+--     10. Drop Constraint
+-- Gru has realized that he needs to give the Minions two days off per week and wants to update the database accordingly. To do this, you need to remove the constraint that prevents the "equipped" column from having a NULL value.
+-- Submit your query for this task in the Judge system.
+
+
+--     11. Remove Column
+-- Dr. Nefario decided to create another table and move some data there. Help him by writing a SQL query to DROP the column "age".
+-- Submit your query for this task in the Judge system.
+
+
     12. Table Birthdays
 Assist Dr. Nefario in creating a new table to store information about the Minions' birthdays and organize surprise parties. Make sure to add constraints to the data types, the "id" must be UNIQUE, and be cautious of non-NULL and DEFAULT values. The table should be named "minions_birthdays" and include the following columns:
     • "id";
